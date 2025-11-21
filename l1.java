9b9d9428aeedb9475556ef2c92b7542a529dd941
Yashaswini H P <yashaswinihp23123@gmail.com>
2025-11-21 23:15:22 +0530

    Update l1.java

--- l1.java
+++ l1.java
@@ -237,3 +237,19 @@ class SolutionFourteen {
 }
 
 // -------------------------------------------------------------------------------------
+
+// Remove Duplicates from Sorted Array
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+}
+
+// -------------------------------------------------------------------------------------
