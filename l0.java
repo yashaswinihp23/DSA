29a2a973daafb48d18fcc2855224f3980289dd91
Yashaswini H P <yashaswinihp23123@gmail.com>
2025-12-04 21:54:02 +0530

    Update l0.java

--- l0.java
+++ l0.java
@@ -1796,3 +1796,18 @@ class Solution {
 }
 
 // ------------------------------------------------------------------------------
+
+// First Unique Character in a String
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] f = new int[26];
+        for (char c : s.toCharArray()) f[c - 'a']++;
+        for (int i = 0; i < s.length(); i++)
+            if (f[s.charAt(i) - 'a'] == 1) return i;
+        return -1;
+    }
+}
+
+// ------------------------------------------------------------------------------
+
