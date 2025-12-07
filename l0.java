cbce58c2a0e1929fec6da03e52fd97fe4d6c7469
Yashaswini H P <yashaswinihp23123@gmail.com>
2025-12-07 20:54:53 +0530

    Update l0.java

--- l0.java
+++ l0.java
@@ -1863,3 +1863,25 @@ class Solution {
 }
 
 // ------------------------------------------------------------------------------
+
+// Assign Cookies
+
+import java.util.*;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0, c = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                c++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return c;
+    }
+}
