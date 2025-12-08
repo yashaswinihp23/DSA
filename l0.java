3ed54b59553dc8f41bd76bf51905421199224bc6
Yashaswini H P <yashaswinihp23123@gmail.com>
2025-12-08 21:34:06 +0530

    Update l0.java

--- l0.java
+++ l0.java
@@ -1933,3 +1933,26 @@ class MinStack {
 }
 
 // ------------------------------------------------------------------------------
+
+// Valid Parentheses
+
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') st.push(c);
+            else {
+                if (st.isEmpty()) return false;
+                char t = st.pop();
+                if (c == ')' && t != '(') return false;
+                if (c == ']' && t != '[') return false;
+                if (c == '}' && t != '{') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
+
+// ------------------------------------------------------------------------------
