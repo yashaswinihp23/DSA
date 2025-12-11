ab677c90e69033deb607fe3e5dc605f89279dec7
Yashaswini H P <yashaswinihp23123@gmail.com>
2025-12-11 20:24:03 +0530

    Update l0.java

--- l0.java
+++ l0.java
@@ -1974,5 +1974,20 @@ class Solution {
 
 // ------------------------------------------------------------------------------
 
+// Length of Last Word
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') i--;
+        int c = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            c++;
+            i--;
+        }
+        return c;
+    }
+}
+
 // ------------------------------------------------------------------------------
 
